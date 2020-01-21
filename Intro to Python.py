6542c50c812c74de95e486bf3979a5342e3a2ad1
shivamchauhan25 <shivam25.srgc@gmail.com>
2020-01-21 19:00:24 +0530

    # star programming with print method and variables.

---
+++ Intro to Python.py
@@ -0,0 +1,20 @@
+'''print('* * * *')
+print('* * * *')
+print('* * * *')
+print('* * * *')'''
+
+
+
+'''star='* * * *'
+print(star)
+print(star)
+print(star)
+print(star)'''
+
+'''star='* ' * 10
+print((star+'\n')*10)'''
+
+star4='* * * *'
+star2='*     *'
+#print((star4+((star2+'\n')*2)+(star4)))
+print((star4)+'\n'+((star2+'\n')*2)+(star4))
