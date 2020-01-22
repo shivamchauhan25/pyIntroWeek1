3f566e22fffc1fe2aeaf458b398de873809e4d3e
shivamchauhan25 <shivam25.srgc@gmail.com>
2020-01-22 19:04:33 +0530

    #python Assi-1

---
+++ Operators Assi-1.py
@@ -0,0 +1,53 @@
+#print(1+2*3/4.0)
+#print(11%3)
+#print(7**2)
+#print(2**3)
+'''listRepeat=[1,2,3]
+print(listRepeat*3)'''
+
+'''a='5'
+b='5'
+print(a+b)'''
+'''l=['shivam','a']
+print('a' in l)
+str='shivam'
+print('vam' in str)'''
+
+'''tuple=('a','b','c')
+print('a' in tuple)'''
+
+
+
+#print(not(True and True))
+
+#print(True or False)
+
+#print(not(False and True))
+
+#print(False and False)
+
+'''a=input("Enter the value of a=")
+b=input("Enter the value of b=")
+print(a==b)'''
+
+'''x=12
+y=13
+print(x>10 and y>10)
+print(x>14 or y>14)
+print(not(x>10 and y>10))'''
+
+'''import math
+print(2*(math.pi)*pow(2,2))'''
+
+'''from math import pi,pow
+print(2*(pi)*pow(2,2))'''
+
+
+import random
+rno=random.randint(35,120)
+print(chr(rno))
+
+'''import random
+list=['m1', 'm2','m3']
+print(random.choice(list))'''
+
