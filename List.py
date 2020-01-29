a7c7fa18720e45f2b3f44a3c45880e5d43d09e5f
shivamchauhan25 <shivam25.srgc@gmail.com>
2020-01-29 18:38:10 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,32 @@
+#emptyList=[]
+#print([1,2,3,4])
+
+'''a=[1,2,3]
+b=[4,5]
+print(a+b)
+'''
+#a=[1,2,3]
+#print(a*5)
+
+l1=['a','b','c','d']
+'''print(l1[0])
+print(l1[-1])
+'''
+'''print(l1[0:2])
+print(l1[:3])
+print(l1[1:])
+print(l1[:])
+print(l1[::-1])
+print(l1[0:4:1])
+'''
+
+'''for item in range(len(l1)):
+    print(l1[item])
+
+for item in l1:
+    print(item)
+   '''
+
+
+
+
